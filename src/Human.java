e5fc8fdc11ab2f52e371eaf9d1403cc2846df078
Ario <ariowistus@gmail.com>
2023-01-15 12:50:25 +0100

    Zadanie 3 i 4: get your salary & give yourself a rise

--- src/Human.java
+++ src/Human.java
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime;
+import java.util.Locale;
+
 public class Human {
     String name;
     int age;
@@ -5,4 +8,26 @@ public class Human {
     int height;
     Animal pet;
     Car car;
+    private Double salary;
+
+
+    public Double getSalary() {
+        System.out.println("Data pobrania wynagrodzenia: " + LocalDateTime.now());
+        System.out.println("Wartość wynagrodzenia przed zwróceniem: " + salary);
+        return salary;
+    }
+    public void setSalary(Double salary) {
+        if (salary < 0) {
+            System.out.println("Nie można przypisać ujemnej wartości wypłaty.");
+            return;
+        }
+        System.out.println("Nowe dane zostały wysłane do systemu ");
+        System.out.println("Konieczność odebrania aneksu do umowy od Pani Ani z kadr.");
+        System.out.println("ZUS i US już wiedzą o zmianie wypłaty nie uciekniesz .");
+        this.salary = salary;
+    }
 }
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car("A4", "Audi", "Black", 2010);
 
+        Human human = new Human();
+        human.getSalary();
+        human.setSalary(1000.0);
+        human.getSalary();
+
+
 
 
     }
